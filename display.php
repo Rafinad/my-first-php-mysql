e94e9edc4330a6c5a5fb32f9c67a9ed14791e055
rafinadk <rafinad.uou@gmail.com>
2025-12-12 13:43:58 +0400

    add cookie

--- display.php
+++ display.php
@@ -6,15 +6,29 @@ FROM
 user u INNER JOIN user_has_Group uhg ON u.user_id = uhg.user_user_id
 INNER JOIN mydb.Group g ON g.group_id = uhg.Group_group_id
 GROUP BY u.username";
+
 $username= $_POST["username"];
 $password_from_HTML = $_POST["password"];
-$SelectPassword = "SELECT password FROM user WHERE username='$username'";
+$SelectPassword = "SELECT user_id, password FROM user WHERE username='$username'AND password = '$password_from_HTML'";
+
 if($result = $SQLmydb->query($SelectPassword)){
     foreach($result as $row){
         $password_from_SQL = $row["password"];
+        $user_id = $row["user_id"];
     }
 }
 if($password_from_HTML == $password_from_SQL) $authorization = true;
-if($authorization == true) echo "<p>"."Успешная авторизация"."</p>"."<p>"."Привет,$username"."</p>";
+else $authorization = false;
+if($authorization == true) {
+    $randomBytes = random_bytes(16);
+    $hashUser = bin2hex($randomBytes);
+    $now = time();
+    $expired = $now+10;
+    $expires_at = date("Y-m-d H:i:s", $expired);
+    $SQLmydb->query("INSERT INTO session (user_user_id, session_hash,expires_at) VALUES ($user_id,'$hashUser','$expires_at')");
+    setcookie("user_id", $hashUser);
+    echo "<p>"."Успешная авторизация"."</p>"."<p>"."<a href='profile.php'>Профиль пользователя</a>"."</p>";
+}
 else echo "<p>"."Ошибка авторизации"."</p>"."<p>"."<a href='index.html'>На главную</a>"."</p>";
+
 ?>
---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php
+$SQLmydb = mysqli_connect("127.0.0.1", "rafinadik", "Password1","mydb",3306);
+$now = date("Y-m-d H:i:s", time());
+if (isset($_COOKIE["user_id"])) {
+    $user_hash = $_COOKIE["user_id"];
+    $ALLinfoUser = 
+    "SELECT * 
+    FROM 
+    user u 
+    INNER JOIN 
+    session s 
+    ON u.user_id = s.user_user_id 
+    WHERE session_hash = '$user_hash'";
+    if($result = $SQLmydb->query($ALLinfoUser)){
+        foreach($result as $row){
+            $username = $row["username"];
+            $name = $row["name"];
+            $surname = $row["surname"];
+            $email = $row["email"];
+            $expires_at = $row["expires_at"];
+        }
+    if($expires_at < $now){
+        echo "<p>"."Сессия закрыта"."</p>"."<p>"."<a href='index.html'>На главную</a>"."</p>";
+    }
+    else{
+        echo 
+    "<p>"."username: $username"."<p>".
+    "<p>"."name: $name"."<p>".
+    "<p>"."surname: $surname"."<p>".
+    "<p>"."email: $email"."<p>";
+    }
+}
+}
+else echo "<p>"."Cookie не найден"."</p>"."<p>"."<a href='index.html'>На главную</a>"."</p>";
